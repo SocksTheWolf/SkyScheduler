15e552f1ceab40b92d7be60528a369495bb91c70
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2025-07-06 23:33:51 -0700

    Redirect to dashboard on login
    yay

--- src/index.tsx
+++ src/index.tsx
@@ -118,6 +118,9 @@ app.post("/post/:id/delete", authMiddleware, async (c) => {
 
 // Root route with login form
 app.get("/", (c) => {
+  if (c.get("user") !== null)
+    return c.redirect("/dashboard");
+
   return c.html(<Home />);
 });
 
