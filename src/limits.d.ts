9e618c722de409f32ce3ed4858963f1b55ecae42
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2025-07-10 11:11:51 -0700

    Update limits.d.ts
    Properly fix the math here
    
    Signed-off-by: SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>

--- src/limits.d.ts
+++ src/limits.d.ts
@@ -13,14 +13,15 @@ export const BSKY_MAX_HEIGHT: number = 2000;
 export const BSKY_FILE_SIZE_LIMIT_IN_MB: number = 1;
 export const BSKY_MIN_USERNAME_LENGTH: number = 4;
 export const BSKY_MAX_APP_PASSWORD_LENGTH: number = 20;
-export const CF_FILE_SIZE_LIMIT_IN_MB: number = 70;
-export const CF_MAX_DIMENSION: number = 10000;
 
 export const TO_MB: number = 1024 * 1024;
 export const TO_MiB: number = 1000 * 1000;
 export const R2_FILE_SIZE_LIMIT_IN_MB: number = 100;
 export const BSKY_FILE_SIZE_LIMIT: number = BSKY_FILE_SIZE_LIMIT_IN_MB * TO_MiB;
 export const R2_FILE_SIZE_LIMIT: number = R2_FILE_SIZE_LIMIT_IN_MB * TO_MB;
+export const CF_FILE_SIZE_LIMIT_IN_MB: number = 70;
+export const CF_FILE_SIZE_LIMIT: number = CF_FILE_SIZE_LIMIT_IN_MB * TO_MB;
+export const CF_MAX_DIMENSION: number = 10000;
 
 export const MIN_DASHBOARD_PASS: number = 8;
 export const MAX_DASHBOARD_PASS: number = 30;
