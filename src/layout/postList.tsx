1676d61d94938330e99fe68e6b05d64996593f35
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2025-12-28 20:27:14 -0800

    use regex for optimization

--- src/layout/postList.tsx
+++ src/layout/postList.tsx
@@ -21,23 +21,25 @@ export function ScheduledPost(props: any) {
   const postID = randomstring.generate(7);
   const content: Post = props.post;
   const username: string = props.user;
+  const hasBeenPosted: boolean = (content.posted === true && content.uri !== undefined);
+  const postURIID: string|null = content.uri ? content.uri.match(/\/((?=[^.]*$).+)/)![0] : null;
 
   return html`
   <article>
     <div id="post${postID}">
-      ${content.posted ? '' : raw(`<button type="submit" hx-delete="/post/delete/${content.postid}" hx-confirm="Are you sure you want to delete this post?" hx-target="#posts" hx-swap="innerHTML" hx-trigger="click" class="btn-sm btn-error outline btn-delete">
+      ${hasBeenPosted ? '' : raw(`<button type="submit" hx-delete="/post/delete/${content.postid}" hx-confirm="Are you sure you want to delete this post?" hx-target="#posts" hx-swap="innerHTML" hx-trigger="click" class="btn-sm btn-error outline btn-delete">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width="20px" height="20px">
           <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
         </svg>
       </button>`)}
-      <div ${content.posted ? '' : raw(`title="Click to edit post content" hx-get="/post/edit/${content.postid}" hx-trigger="click once" hx-target="#post${postID}" hx-swap="innerHTML"`)}>
+      <div ${hasBeenPosted ? '' : raw(`title="Click to edit post content" hx-get="/post/edit/${content.postid}" hx-trigger="click once" hx-target="#post${postID}" hx-swap="innerHTML"`)}>
         ${<PostContentObject text={content.text}/>}
       </div>
     </div>
     <footer>
       <small>
-        ${ (content.posted && content.uri) ? 
-          html`<a class="secondary" href="https://bsky.app/profile/${username}/post/${content.uri.slice(content.uri.lastIndexOf('/') + 1)}" target="_blank" title="link to post">Posted on</a>:` : 
+        ${hasBeenPosted ? 
+          raw(`<a class="secondary" data-uri="${content.uri}" href="https://bsky.app/profile/${username}/post${postURIID}" target="_blank" title="link to post">Posted on</a>:`) : 
           'Scheduled for:' } 
           <span class="timestamp">${content.scheduledDate}</span>${content.repostCount! ? ' | Reposts Left: ' + content.repostCount : ''}
       </small>
