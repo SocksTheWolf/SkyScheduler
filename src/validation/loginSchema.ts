de06d8cdfd077de560eeeaea602374cf407ed129
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2025-12-28 18:50:11 -0800

    login and signup validation simplify

--- src/validation/loginSchema.ts
+++ src/validation/loginSchema.ts
@@ -1,14 +1,8 @@
 import * as z from "zod/v4";
-import { BSKY_MIN_USERNAME_LENGTH, MAX_DASHBOARD_PASS, MIN_DASHBOARD_PASS } from "../limits.d";
+import { PasswordSchema, UsernameSchema } from "./sharedValidations";
 
 // Schema for login validation
 export const LoginSchema = z.object({
-  username: z.string().trim().toLowerCase()
-    .nonempty("username is missing")
-    .min(BSKY_MIN_USERNAME_LENGTH, "username too short")
-    .regex(z.regexes.domain, "username must be in the format of username.bsky.social or a custom domain"),
-  password: z.string().trim()
-    .nonempty("password is missing")
-    .min(MIN_DASHBOARD_PASS, "password too short")
-    .max(MAX_DASHBOARD_PASS, "password too long"),
+  ...UsernameSchema.shape,
+  ...PasswordSchema.shape,
 });
--- src/validation/signupSchema.ts
+++ src/validation/signupSchema.ts
@@ -1,19 +1,9 @@
 import * as z from "zod/v4";
-import { appPasswordRegex } from "./regexCases";
-import { BSKY_MAX_APP_PASSWORD_LENGTH, BSKY_MIN_USERNAME_LENGTH, MAX_DASHBOARD_PASS, MIN_DASHBOARD_PASS } from "../limits.d";
+import { PasswordSchema, UsernameSchema, BSkyAppPasswordSchema } from "./sharedValidations";
 
 export const SignupSchema = z.object({
+  ...UsernameSchema.shape,
+  ...PasswordSchema.shape,
+  ...BSkyAppPasswordSchema.shape,
   signupToken: z.string().trim().optional(),
-  username: z.string().trim().toLowerCase()
-    .nonempty("username is missing")
-    .min(BSKY_MIN_USERNAME_LENGTH, "username too short")
-    .regex(z.regexes.domain, "username should be in a format like username.bsky.social or a domain"),
-  password: z.string().trim()
-    .nonempty("password is missing")
-    .min(MIN_DASHBOARD_PASS, "password too short")
-    .max(MAX_DASHBOARD_PASS, "password too long"),
-  bskyAppPassword: z.string().trim()
-    .nonempty("missing bsky app password")
-    .max(BSKY_MAX_APP_PASSWORD_LENGTH, "app password too long")
-    .regex(appPasswordRegex, "please go back and recreate your app password from your bsky settings")
 });
