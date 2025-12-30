0ec359c8f2db42626a8f2f173f07965eb25b877f
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2025-12-29 23:35:52 -0800

    update the homepage with new things
    add more info wow

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,16 +1,16 @@
 import FooterCopyright from "../layout/footer";
 import { BaseLayout } from "../layout/main";
 import NavTags from "../layout/navTags";
-import { MAX_HOURS_REPOSTING, MAX_REPOST_INTERVAL } from "../limits.d";
+import { MAX_HOURS_REPOSTING, MAX_REPOST_INTERVAL, CF_FILE_SIZE_LIMIT_IN_MB } from "../limits.d";
 
 export default function Home() {
   return (
-    <BaseLayout title="SkyScheduler - Home">
+    <BaseLayout title="SkyScheduler - Home" mainClass="homepage">
       <NavTags />
       <section class="container">
         <article>
           <p>
-            <strong>SkyScheduler</strong> is an <a href="https://github.com/socksthewolf/skyscheduler" rel="nofollow" target="_blank">open source</a> service that 
+            <strong>SkyScheduler</strong> is a free, <a href="https://github.com/socksthewolf/skyscheduler" rel="nofollow" target="_blank">open source</a> service that 
             lets you schedule and automatically repost your content on Bluesky!<br />
             Boost engagement and reach more people no matter what time of day!<br />
             <center>
@@ -28,10 +28,10 @@ export default function Home() {
             </center>
             <h6 class="inline-header">Features:</h6>
             <ul>
-              <li>Handles multiple users/accounts easily</li>
+              <li>Handles multiple users/accounts easily, supports custom PDS instances</li>
               <li>Schedule your posts any time in the future (to the nearest hour)</li>
               <li>Supports embeds, links, tagging, mentions</li>
-              <li>Post images with content labels and full support for alt text</li>
+              <li>Post <span data-tooltip={`images and video (up to ${CF_FILE_SIZE_LIMIT_IN_MB} MB)`}>media</span> with content labels and full support for alt text</li>
               <li>Automatically repost your posts at an interval of your choosing, up to {MAX_REPOST_INTERVAL-1} times every {MAX_HOURS_REPOSTING-1} hours</li>
               <li>Edit the content of pending posts before posting</li>
             </ul>
