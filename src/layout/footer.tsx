9099f26f8d8538410224b7a955152945176030dd
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-01-04 18:34:58 -0800

    modify credits layout

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,14 +1,16 @@
 // Helper footer for various pages
 export default function FooterCopyright() {
   return (
-    <small> 
+    <center><small> 
       <a class="secondary" target="_blank" title="project source on github" href="https://github.com/SocksTheWolf/SkyScheduler">SkyScheduler</a> &copy; {new Date().getFullYear()} 
       <span class="credits">
-        <a rel="author" title="project author" href="https://socksthewolf.com">SocksTheWolf</a> -
-        <a class="secondary" target="_blank" title="tip project maintainer" href="https://ko-fi.com/socksthewolf">Tip</a> - 
-        <a class="secondary" href="/tos" title="Terms of Service document">Terms</a> - 
-        <a class="secondary" href="/privacy" title="Privacy Policy document">Privacy</a>
+        <a rel="author" title="project author" href="https://socksthewolf.com">SocksTheWolf</a><br />
+        <small>
+          <a class="secondary" target="_blank" title="tip project maintainer" href="https://ko-fi.com/socksthewolf">Tip</a> - 
+          <a class="secondary" href="/tos" title="Terms of Service document">Terms</a> - 
+          <a class="secondary" href="/privacy" title="Privacy Policy document">Privacy</a>
+        </small>
       </span>
-    </small>
+    </small></center>
   );
 }
