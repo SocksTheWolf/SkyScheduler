a83aa5530f809f701f96145ac8de973a7bca0ae9
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-01-30 01:53:31 -0800

    make the UI consistent across all devices
    FALCON YES

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -71,11 +71,11 @@ export default function Dashboard(props:any) {
         <div class="col-9">
           <ViolationNoticeBar ctx={ctx} />
           <div role="tablist">
-            <button id="tabone" role="tab" aria-selected="true" aria-controls="postTab">New Post</button>
+            <button id="tabone" class="w-half" role="tab" aria-selected="true" aria-controls="postTab">New Post</button>
+            <button id="tabtwo" class="w-half" role="tab" aria-controls="repostTab">New Retweet</button>
             <div id="postTab" role="tabpanel" aria-labelledby="tabone" >
               <PostCreation />
             </div>
-            <button id="tabtwo" role="tab" aria-controls="repostTab">New Retweet</button>
             <div id="repostTab" role="tabpanel" aria-labelledby="tabtwo" hidden>
               <MakeRetweet />
             </div>
