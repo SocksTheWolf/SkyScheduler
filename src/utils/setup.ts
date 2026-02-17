8be952c0ba5fbe6094090daa1b26a94632ca69cc
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-02-16 19:46:11 -0800

    Check for invalid config setup
    Signed-off-by: SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>

--- src/utils/setup.ts
+++ src/utils/setup.ts
@@ -1,9 +1,13 @@
 import { Context } from "hono";
 import { doesAdminExist } from "./db/userinfo";
+import has from "just-has";
 
 export const setupAccounts = async(c: Context) => {
   if (await doesAdminExist(c))
     return c.html("already created", 501);
+
+  if (!has(c.env, "DEFAULT_ADMIN_USER") || !has(c.env, "DEFAULT_ADMIN_PASS") || !has(c.env, "DEFAULT_ADMIN_BSKY_PASS"))
+    return c.html("invalid configuration, missing configs");
 
   const data = await c.get("auth").api.signUpEmail({
     body: {
