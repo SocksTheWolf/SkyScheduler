94803196c4fb9925088d9edf6822ca2bcbcf2902
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-02-20 20:04:31 -0800

    forgot to save lol

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -6,6 +6,7 @@ import { drizzle, DrizzleD1Database } from "drizzle-orm/d1";
 import { schema } from "../db";
 import { BSKY_MAX_USERNAME_LENGTH, BSKY_MIN_USERNAME_LENGTH } from "../limits";
 import { APP_NAME } from "../siteinfo";
+import { Bindings } from "../types";
 import { lookupBskyHandle } from "../utils/bskyApi";
 import { createDMWithUser } from "../utils/bskyMsg";
 
