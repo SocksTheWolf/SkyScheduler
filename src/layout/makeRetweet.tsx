2c9192ca7cb8a1ed1bd5477a9196145d544b88f8
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-02-28 22:17:53 -0800

    make ui more correct/pretty
    good yes

--- src/layout/makeRetweet.tsx
+++ src/layout/makeRetweet.tsx
@@ -12,12 +12,12 @@ export function MakeRetweet() {
       <header>Post To Retweet</header>
       <input type="text" id="repostRecordURL" placeholder="https://" />
       <small>This must be a post, it cannot be anything else.
-        It must also exist, and be reachable
+        The post must also exist, and be reachable at the scheduled time
         (i.e. a post that's not deleted, nor are you forbidden from seeing it)</small>
       <blockquote id="repostTitleSection">
-        <label for="repostTitle">Repost Title (Optional)</label>
-        <input type="text" id="repostTitle" maxlength={MAX_POSTED_LENGTH} />
-        <small>A name or simple phrase to help you organize reposts. Max {MAX_POSTED_LENGTH} characters. Will update existing titles.</small>
+        <label for="repostTitle">Retweet Title <small>(Optional)</small></label>
+        <input type="text" id="repostTitle" maxlength={MAX_POSTED_LENGTH} placeholder="Leave blank to use existing/generated title" />
+        <small>A name or simple phrase to help you organize retweets. Max {MAX_POSTED_LENGTH} characters. Will update existing titles, cannot be used on scheduled posts.</small>
       </blockquote>
     </article>
     <ScheduleOptions timeID="repostTime" allowNow={false} type="retweet" header="Retweet At" />
