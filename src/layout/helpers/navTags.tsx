75f8e0f433d4c09efd6993631fe328e9ccef4208
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-03-05 19:18:22 -0800

    attempt to make accessibility better
    i love css wow

--- src/layout/helpers/navTags.tsx
+++ src/layout/helpers/navTags.tsx
@@ -8,14 +8,14 @@ export default function NavTags() {
         <div>
           <a href="/" title={`${APP_NAME} homepage`}>
             <LogoImage width={64} height={64} />
-            <h2 class="secondary">{APP_NAME}</h2>
+            <h2 class="appTitle">{APP_NAME}</h2>
           </a>
         </div>
         <div style="flex-grow: 1"></div>
         <nav>
           <ul>
-            <li><a href="/signup">Sign Up</a></li>
-            <li><a href="/login"><button class="secondary">Login</button></a></li>
+            <li><a role="button" href="/signup">Sign Up</a></li>
+            <li><a role="button" class="login secondary" href="/login">Login</a></li>
           </ul>
         </nav>
       </div>
