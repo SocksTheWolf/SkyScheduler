24d467c53eee72c2f8535aa4a17db8922903c4f0
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-03-14 22:35:08 -0700

    expose the retry header

--- src/middleware/corsHelper.ts
+++ src/middleware/corsHelper.ts
@@ -8,7 +8,7 @@ export const corsHelperMiddleware = async (c: Context, next: any) => {
     allowMethods: ["POST", "GET", "OPTIONS", "DELETE"],
     exposeHeaders: ["Content-Length", "Content-Type", "HX-Trigger",
       "HX-Push-Url", "HX-Replace-Url", "HX-Refresh", "Cache-Control",
-      "HX-Redirect", "HX-Trigger-After-Swap", "HX-Trigger-After-Settle"],
+      "HX-Redirect", "HX-Trigger-After-Swap", "HX-Trigger-After-Settle", "X-Retry-After"],
     maxAge: 600,
     credentials: true,
   });
