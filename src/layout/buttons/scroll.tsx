23628b5261fa94b17318961c163df82be82ca607
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-03-19 16:49:55 -0700

    make green a little

--- src/layout/buttons/scroll.tsx
+++ src/layout/buttons/scroll.tsx
@@ -1,8 +1,8 @@
 export default function ScrollToContent() {
   return (<>
-  <button class="outline contrast scrollBtn">
+  <button class="outline scrollBtn">
     <span>Make New Post</span>
-    <img src="/icons/new-post.svg" height="20px" width="20px" alt="new post button" />
+    <img src="/icons/new-post.svg" height="20px" width="20px" alt="scroll to making new post" />
   </button>
   </>);
 }
