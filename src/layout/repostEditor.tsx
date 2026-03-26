ee17b2d6509804d4d641f6b1554cc425d48ef6cd
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-03-25 22:27:20 -0700

    Improve retweet editor UI
    * fits better on mobile
    * has good hoverovers
    * doesn't render table if no data
    
    so many small improvements

--- src/layout/repostEditor.tsx
+++ src/layout/repostEditor.tsx
@@ -4,12 +4,12 @@ import { RepostInfo } from "../classes/repost";
 import { AllContext } from "../types";
 import { getPostById } from "../utils/dbQuery";
 
-type RepostEditorProps = {
+type RepostEditorTableProps = {
   id: string;
   data?: RepostInfo[];
 };
 
-export function RepostEditorData(props: RepostEditorProps) {
+function RepostEditorTable(props: RepostEditorTableProps) {
   if (props.data === undefined || !isValid(props.id)) {
     return (<>No Reposts To Edit</>);
   }
@@ -18,20 +18,31 @@ export function RepostEditorData(props: RepostEditorProps) {
     if (a.time > b.time) return 1;
     return 0;
   });
-  // The hx-confirm will get injected once the timestamps are localized on the client.
-  return (<>{sortedData.map((obj) => {
-    const isMultiRetweet: boolean = obj.hours > 0;
-    return (<tr class="repost-editor-item">
-      <th scope="row" class="timestamp">{obj.time}</th>
-      <td>{isMultiRetweet ? `Every ${obj.hours} hours` : "once"}</td>
-      <td>{isMultiRetweet ? obj.count : 0} + 1 (initial)</td>
-      <td><a role="button" hx-delete={`/post/${props.id}/repost/${obj.guid}`}
-        hx-indicator="#repostDataPopoverProgress"
-        hx-disabled-elt=".repost-editor-item a"
-        hx-target="closest tr" hx-swap="delete">Delete</a></td>
-    </tr>);
-  })}</>);
-};
+  return (<table class="striped">
+    <thead>
+      <tr>
+        <th scope="col">Repost Start Date</th>
+        <th scope="col">Repost Cadance</th>
+        <th scope="col"><span data-tooltip="This field does not update with the number of reposts that have been made">Approx Num Reposts</span></th>
+        <th scope="col">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {sortedData.map((obj) => {
+        const isMultiRetweet: boolean = obj.hours > 0;
+        return (<tr class="repost-editor-item">
+          <th scope="row" class="timestamp">{obj.time}</th>
+          <td>{isMultiRetweet ? `Every ${obj.hours} hours` : "once"}</td>
+          <td>{isMultiRetweet ? obj.count : 0} + 1 (initial)</td>
+          <td><a role="button" hx-delete={`/post/${props.id}/repost/${obj.guid}`}
+            hx-indicator="#repostDataPopoverProgress"
+            hx-disabled-elt=".repost-editor-item a"
+            hx-target="closest tr" hx-swap="delete">Delete</a></td>
+        </tr>);
+      })}
+    </tbody>
+  </table>);
+}
 
 type RepostDataPopoverProps = {
   ctx: AllContext;
@@ -49,20 +60,9 @@ export async function RepostDataPopover(props: RepostDataPopoverProps) {
     <header>
       <h2>Edit Repost Rules</h2>
     </header>
-    <p>Here are the current repost rules for the given post. If a rule overlaps another, the first rule wins.</p>
-    <table class="striped">
-      <thead>
-        <tr>
-          <th scope="col">Repost Start Date</th>
-          <th scope="col">Repost Cadance</th>
-          <th scope="col">Approx Num Reposts</th>
-          <th scope="col">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <RepostEditorData id={props.id} data={repostInfoData?.repostInfo} />
-      </tbody>
-    </table>
+    <p>Here are the current repost rules for the given post.</p>
+    <p><b>NOTE</b>: If a schedule rule overlaps with another, then only the first rule has a registered action, and any duplicate scheduled action is not created.</p>
+    <RepostEditorTable id={props.id} data={repostInfoData?.repostInfo} />
     <progress id="repostDataPopoverProgress" class="htmx-indicator" />
     <footer>
       <small id="click-close" class="clicker noselect" tabindex={0}>Click to close this editor</small>
