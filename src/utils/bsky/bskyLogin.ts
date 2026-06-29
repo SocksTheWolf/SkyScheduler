b6b359d64bc5327c04cb4ce0533d14c85817426e
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-06-28 23:21:43 -0700

    Handle account status checks better
    success could be true, but active could be false, and thus bypassing flags that we should be checking for.

--- src/utils/bsky/bskyLogin.ts
+++ src/utils/bsky/bskyLogin.ts
@@ -1,9 +1,25 @@
 // handles general login for agents.
 // most cases should not be interfacing directly with this function, but instead
 // use bskyAgents or bskyMsg to talk to the network
+import type { ComAtprotoServerCreateSession } from "@atproto/api";
 import type { AtProtoAgent } from "../../classes/bskyAgents";
 import { AccountStatus } from "../../enums";
 import type { LooseObj } from "../../types";
+
+const checkAccountStatus = (data: ComAtprotoServerCreateSession.OutputSchema) => {
+  if (data.active === false) {
+    switch (data.status) {
+      case "deactivated":
+        return AccountStatus.Deactivated;
+      case "suspended":
+        return AccountStatus.Suspended;
+      case "takendown":
+        return AccountStatus.TakenDown;
+    }
+    return AccountStatus.InvalidAccount;
+  }
+  return AccountStatus.Ok;
+}
 
 export const loginToBsky = async (agent: AtProtoAgent, user: string, pass: string) => {
   try {
@@ -13,20 +29,15 @@ export const loginToBsky = async (agent: AtProtoAgent, user: string, pass: strin
       allowTakendown: true
     });
     if (!loginResponse.success) {
-      if (loginResponse.data.active == false) {
-        switch (loginResponse.data.status) {
-          case "deactivated":
-            return AccountStatus.Deactivated;
-          case "suspended":
-            return AccountStatus.Suspended;
-          case "takendown":
-            return AccountStatus.TakenDown;
-        }
-        return AccountStatus.InvalidAccount;
-      }
-      return AccountStatus.PlatformOutage;
+      const failedCheck: AccountStatus = checkAccountStatus(loginResponse.data);
+      // If the account was ok, that means we currently are having a platform outage
+      if (failedCheck === AccountStatus.Ok)
+        return AccountStatus.PlatformOutage;
+      // otherwise there's something up with the account
+      return failedCheck;
     }
-    return AccountStatus.Ok;
+    // check account status if we succeed, because we could get a valid return-code but account disabled
+    return checkAccountStatus(loginResponse.data);
   } catch (err) {
     // Apparently login can rethrow as an XRPCError and completely eat the original throw.
     // so errors don't get handled gracefully.
