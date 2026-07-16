df0d84c5ddda3d5d8d676cd46386b3a331665a94
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-07-15 17:05:52 -0700

    fix type import

--- src/classes/bskyConvoInfo.ts
+++ src/classes/bskyConvoInfo.ts
@@ -1,11 +1,11 @@
-import type { ConvoView } from '@atproto/api/dist/client/types/chat/bsky/convo/defs';
+import type { ChatBskyConvoDefs } from "@atproto/api";
 
 export class BSkyConvoInfo {
   id: string;
   unreadCount: number;
   isRequest: boolean;
   hasLastMessage: boolean;
-  constructor(convo: ConvoView) {
+  constructor(convo: ChatBskyConvoDefs.ConvoView) {
     this.id = convo.id;
     this.isRequest = (convo.status === "request") || false;
     this.hasLastMessage = convo.lastMessage !== undefined;
