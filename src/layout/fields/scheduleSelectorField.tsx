ac5c07760e469b70b4aeae58d2a30cbe7a4659e5
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-07-18 22:47:04 -0700

    Setup partial fragment render
    for the repost times/intervals

---
+++ src/layout/fields/scheduleSelectorField.tsx
@@ -0,0 +1,36 @@
+import { MAX_REPOST_IN_HOURS, MAX_REPOST_INTERVAL_LIMIT, REPOSTING_TIME_INTERVAL } from "../../limits";
+import { formatTime } from "../../utils/helpers";
+
+// These are really not optimal, and they can also run twice. Which is not great.
+// A way to render them once and then cache that would be fantastic, but it's unclear what
+// would be the most performant option.
+
+export function RepostTimeSelector() {
+  const subIntervalLimit: number = 60 / REPOSTING_TIME_INTERVAL;
+  return (<select disabled>
+    {[...Array(MAX_REPOST_IN_HOURS)].map((x, i) => {
+      if (i == 0) return;
+      const dayField = Math.floor(i/24);
+      const hourField = i % 24;
+      // This array is pure evil and ugly and horrible
+      let evilArray = [];
+      for (let t = 0; t < subIntervalLimit; ++t) {
+        const minutesValue = t * (REPOSTING_TIME_INTERVAL as number);
+        evilArray.push(<option value={i + minutesValue/60}>{formatTime(dayField, hourField, minutesValue)}</option>);
+        // When at limit, we should not add sub options, so break out.
+        if (i == MAX_REPOST_IN_HOURS-1)
+          break;
+      }
+      return evilArray;
+    })}
+  </select>);
+};
+
+export function RepostAmountSelector() {
+  return (<select disabled>
+    {[...Array(MAX_REPOST_INTERVAL_LIMIT)].map((x, i) => {
+      if (i == 0) return;
+      return (<option value={i}>{i} times</option>);
+    })}
+  </select>);
+}
--- src/layout/options/repostOptions.tsx
+++ src/layout/options/repostOptions.tsx
@@ -1,7 +1,6 @@
 import isEmpty from "just-is-empty";
-import { MAX_REPOST_IN_HOURS, MAX_REPOST_INTERVAL_LIMIT, REPOSTING_TIME_INTERVAL } from "../../limits";
 import type { BaseElementProps } from "../../types";
-import { formatTime } from "../../utils/helpers";
+import { RepostAmountSelector, RepostTimeSelector } from "../fields/scheduleSelectorField";
 
 type RepostOptionsProps = BaseElementProps & {
   id: string;
@@ -15,35 +14,13 @@ type RepostOptionsProps = BaseElementProps & {
 export default function RepostOptions(props: RepostOptionsProps) {
   const repostedFrom = !isEmpty(props.timeString) ? props.timeString : "the post time";
   const checkboxLabel = !isEmpty(props.checkboxLabel) ? props.checkboxLabel : "Should Auto-Retweet?";
-  const subIntervalLimit: number = 60 / REPOSTING_TIME_INTERVAL;
   return (<section>
     <input class="autoRepostBox" type="checkbox" id={props.id} hidden={props.hidden} startchecked={props.checked} />
     <label hidden={props.hidden} class="noselect checkboxLine" for={props.id}>{checkboxLabel}</label>
     <fieldset class="repostScheduleSimple" role="group">
       <span class="noselect">Automatically repost this {props.contentType || "content"} every</span>
-      <select disabled>
-        {[...Array(MAX_REPOST_IN_HOURS)].map((x, i) => {
-          if (i == 0) return;
-          const dayField = Math.floor(i/24);
-          const hourField = i % 24;
-          // This array is pure evil and ugly and horrible
-          let evilArray = [];
-          for (let t = 0; t < subIntervalLimit; ++t) {
-            const minutesValue = t * (REPOSTING_TIME_INTERVAL as number);
-            evilArray.push(<option value={i + minutesValue/60}>{formatTime(dayField, hourField, minutesValue)}</option>);
-            // When at limit, we should not add sub options, so break out.
-            if (i == MAX_REPOST_IN_HOURS-1)
-              break;
-          }
-          return evilArray;
-        })}
-      </select>
-      <select disabled>
-        {[...Array(MAX_REPOST_INTERVAL_LIMIT)].map((x, i) => {
-          if (i == 0) return;
-          return (<option value={i}>{i} times</option>);
-        })}
-      </select>
+      <RepostTimeSelector />
+      <RepostAmountSelector />
       <span class="noselect">from {repostedFrom}.</span>
     </fieldset>
   </section>);
