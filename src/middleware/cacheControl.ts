39e8a354d2b9ff4a9c131953dbf81c482151df71
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-07-30 18:58:03 -0700

    add cache tags and cache settings

--- src/middleware/cacheControl.ts
+++ src/middleware/cacheControl.ts
@@ -2,16 +2,19 @@ import type { Context } from "hono";
 
 // Instead of using hono cache, we use CF's worker cache.
 export async function cachePublicMiddleware(c: Context, next: any) {
+  c.res.headers.set("Cache-Tag", "static");
   c.res.headers.set("Cache-Control", "public, max-age=600, stale-while-revalidate=3600");
   await next();
 };
 
 export async function cacheStaticFileMiddleware(c: Context, next: any) {
-  c.res.headers.set("Cache-Control", 'public, max-age=604800, stale-while-revalidate=100000, must-revalidate, proxy-revalidate');
+  c.res.headers.set("Cache-Tag", "staticfiles,static");
+  c.res.headers.set("Cache-Control", "public, max-age=604800, stale-while-revalidate=100000, immutable, must-revalidate");
   await next();
 };
 
 export async function cachePrivateMiddleware(c: Context, next: any) {
-  c.res.headers.set("Cache-Control", "private");
+  c.res.headers.set("Cache-Tag", "private");
+  c.res.headers.set("Cache-Control", "private, max-age=200, must-revalidate");
   await next();
 };
