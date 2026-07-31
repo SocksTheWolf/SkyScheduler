42dddf22f326f9c6cf4eb84ccadee3226487d0b1
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-07-30 23:29:47 -0700

    unlink openapi file if it fails for whatever reason

--- bin/openapi.ts
+++ bin/openapi.ts
@@ -1,12 +1,21 @@
+import fs from "fs/promises";
+import { existsSync } from "node:fs";
 import { generateOpenAPI } from "../src/endpoints/openapi";
-import fs from 'fs/promises';
 
 async function buildOpenAPISpec() {
+  const openapiPath: string = "./openapi.json";
   try {
     const spec = JSON.stringify(await generateOpenAPI());
-    await fs.writeFile("./openapi.json", spec);
+
+    await fs.writeFile(openapiPath, spec);
+
     console.log("openapi spec wrote");
   } catch(err) {
+    // this is improbable, but if we somehow wrote anything, we need to
+    // get rid of it.
+    if (existsSync(openapiPath)) {
+      await fs.unlink(openapiPath);
+    }
     console.error(`failed to write openapi spec, got error ${err}`);
     throw new Error("failed");
   }
