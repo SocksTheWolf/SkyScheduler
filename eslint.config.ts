65bb3caa00c396957bfd67d3db4190ac2d3df0ad
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-08-12 19:00:51 -0700

    more project cleanup

--- eslint.config.ts
+++ eslint.config.ts
@@ -7,46 +7,43 @@ import tseslint from "typescript-eslint";
 import { configObject } from "./jsrules.config";
 
 export default defineConfig([
-  globalIgnores([".wrangler/**", ".github/**", "assets/dep/**", ".vscode/**", "dist/**", "src/definitions/wrangler.d.ts", "*.config.ts", "assets/js/min/**"]),
+  globalIgnores([
+    ".wrangler/**",
+    ".github/**",
+    "assets/dep/**",
+    ".vscode/**",
+    "dist/**",
+    "src/definitions/wrangler.d.ts",
+    "*.config.ts",
+    "assets/js/min/**",
+  ]),
   // ESLint for Typescript
   {
     files: ["src/**.{ts,tsx}"],
     plugins: { js },
     extends: ["js/recommended", tseslint.configs.strictTypeChecked, baseConfig],
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-      }
-    },
+    languageOptions: { parserOptions: { projectService: true } },
     rules: {
-      "curly": "off",
+      curly: "off",
       // hono gets
       "@typescript-eslint/no-unsafe-assignment": "off",
       // necessary for valid, uuidValid, has, isEmpty, etc
       "@typescript-eslint/no-non-null-assertion": "off",
       "@typescript-eslint/no-unnecessary-condition": "error",
       // ts ignore annoyances
-      "@typescript-eslint/ban-ts-comment": "off"
-    }
+      "@typescript-eslint/ban-ts-comment": "off",
+    },
   },
   // ESLint for website JS
   {
     files: ["assets/js/**.js"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: {
-      sourceType: "commonjs",
-      globals: {
-        ...globals.browser,
-        ...configObject,
-      }
-    },
+    languageOptions: { sourceType: "commonjs", globals: { ...globals.browser, ...configObject } },
     rules: {
       "no-control-regex": "off",
       "no-unused-vars": "off",
-      "no-useless-escape": ["error", {
-        "allowRegexCharacters": ["-", ".", ":"]
-      }]
-    }
-  }
+      "no-useless-escape": ["error", { allowRegexCharacters: ["-", ".", ":"] }],
+    },
+  },
 ]);
