19e1c457905420cd789f6046de6ba562dafa9f70
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-08-31 15:54:33 -0700

    same line again
    this was originally multi-lined because I had more rules but then I removed them.

--- src/layout/buttons/refresh.tsx
+++ src/layout/buttons/refresh.tsx
@@ -2,8 +2,7 @@ import type { BaseElementProps } from "../../types";
 
 export default function RefreshPostsButton(_props?: BaseElementProps) {
   return (<button id="refresh-posts" hx-get="/post/all" hx-target="#posts"
-      hx-trigger="refreshPosts from:body throttle:3s queue:none, accountUpdated from:body,
-        click throttle:5s queue:none, load once"
+      hx-trigger="refreshPosts from:body throttle:3s queue:none, accountUpdated from:body, click throttle:5s queue:none, load once"
       hx-indicator="inherit, .refresh-icon">
     <span>Refresh Content</span>
     <img class="refresh-icon" src="/icons/refresh.svg" height="20px" width="20px" alt="refresh icon" />
