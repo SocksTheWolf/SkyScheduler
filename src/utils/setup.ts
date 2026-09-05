cf1798a4857b7fbd9ea9bfc2ad0b5ceaaaccdb24
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-09-05 09:54:04 -0700

    update setup fields
    fill out with the correct default values

--- src/utils/setup.ts
+++ src/utils/setup.ts
@@ -1,4 +1,5 @@
 import type { BaseContext } from "../types";
+import { getUserDID, getUserPDS } from "./bsky/bskyUser";
 import { doesAdminExist } from "./db/userinfo";
 import { has } from "./helpers";
 
@@ -14,15 +15,24 @@ export const setupAccounts = async (c: BaseContext) => {
       return c.text(`missing ${setting} setting!`);
     }
   }
+  const adminName = c.env.DEFAULT_ADMIN_USER;
+
+  const profileDID: string|null = await getUserDID(adminName);
+  if (profileDID === null) {
+    return c.text(`admin user ${adminName} has an invalid DID`)
+  }
+  const userPDS: string = await getUserPDS(profileDID);
 
   const data = await c.get("auth").api.signUpEmail({
     body: {
       name: "admin",
-      email: `${c.env.DEFAULT_ADMIN_USER}@skyscheduler.tld`,
+      email: `${adminName}@skyscheduler.tld`,
       // @ts-ignore: Property does not exist (it does via an extension)
-      username: c.env.DEFAULT_ADMIN_USER,
+      username: adminName,
       password: c.env.DEFAULT_ADMIN_PASS,
       bskyAppPass: c.env.DEFAULT_ADMIN_BSKY_PASS,
+      did: profileDID,
+      pds: userPDS
     },
   });
   if (data.token !== null)
